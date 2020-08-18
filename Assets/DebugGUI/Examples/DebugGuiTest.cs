840e92f96c4db9b61df1c6b01a93c8e937e0518d
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-08-18 23:02:01 +0300

    minor fixes

---
+++ Assets/DebugGUI/Examples/DebugGuiTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DebugGuiTest : MonoBehaviour
+{
+    [DebugGUIGraph(r: 0, g: 1, b: 0, autoScale: true)]
+    private float bytesPerSec;
+
+    private void Update()
+    {
+        bytesPerSec = Mathf.Sin(Time.time);
+        DebugGUI.Graph("bytesPerSec", bytesPerSec);
+    }
+}
