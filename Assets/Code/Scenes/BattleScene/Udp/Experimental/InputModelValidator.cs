4746ac04c0e7edb6c76d7d3c5fb8d246f9d35a27
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-08-18 16:09:22 +0300

    убрал испочники света из снарядов

--- Assets/Code/Scenes/BattleScene/Udp/Experimental/InputModelValidator.cs
+++ Assets/Code/Scenes/BattleScene/Udp/Experimental/InputModelValidator.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using Code.Common.Logger;
 using NetworkLibrary.NetworkLibrary.Udp.PlayerToServer.UserInputMessage;
 
 namespace Code.Scenes.BattleScene.Udp.Experimental
 {
     public class InputModelValidator
     {
+        private readonly ILog log = LogManager.CreateLogger(typeof(InputModelValidator));
         public void Validate(InputMessageModel model)
         {
             if (model.TickNumber == 0)
             {
-                throw new Exception("Пустой номер тика");
+                log.Error("Пустой номер тика");
             }
         }
     }
